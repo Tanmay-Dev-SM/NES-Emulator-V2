25f99c87019d3b81fa3f0b838af5cb8f0929ced9
Tanmaydevilhunter <tanmatnfsgta@gmail.com>
2025-05-07 21:04:36 -0400

    Add project files.

---
+++ NES Emulator V2/Bus.cpp
@@ -0,0 +1,87 @@
+#include "Bus.h"
+
+Bus::Bus()
+{
+	// Connect CPU to communication bus
+	cpu.ConnectBus(this);
+}
+
+Bus::~Bus()
+{
+}
+
+void Bus::cpuWrite(uint16_t addr, uint8_t data)
+{
+	if (cart->cpuWrite(addr, data))
+	{
+	}
+	else if (addr >= 0x0000 && addr <= 0x1FFF)
+	{
+		cpuRam[addr & 0x07FF] = data;
+	}
+	else if (addr >= 0x2000 && addr <= 0x3FFF)
+	{
+		ppu.cpuWrite(addr & 0x0007, data);
+	}
+	else if (addr >= 0x4016 && addr <= 0x4017)
+	{
+		controller_state[addr & 0x0001] = controller[addr & 0x0001];
+	}
+}
+
+uint8_t Bus::cpuRead(uint16_t addr, bool bReadOnly)
+{
+	uint8_t data = 0x00;
+	if (cart->cpuRead(addr, data))
+	{
+		// Cartridge Address Range
+	}
+	else if (addr >= 0x0000 && addr <= 0x1FFF)
+	{
+		// System RAM Address Range, mirrored every 2048
+		data = cpuRam[addr & 0x07FF];
+	}
+	else if (addr >= 0x2000 && addr <= 0x3FFF)
+	{
+		// PPU Address range, mirrored every 8
+		data = ppu.cpuRead(addr & 0x0007, bReadOnly);
+	}
+	else if (addr >= 0x4016 && addr <= 0x4017)
+	{
+		data = (controller_state[addr & 0x0001] & 0x80) > 0;
+		controller_state[addr & 0x0001] <<= 1;
+	}
+
+	return data;
+}
+
+void Bus::insertCartridge(const std::shared_ptr<Cartridge>& cartridge)
+{
+	// Connects cartridge to both Main Bus and CPU Bus
+	this->cart = cartridge;
+	ppu.ConnectCartridge(cartridge);
+}
+
+void Bus::reset()
+{
+	cart->reset();
+	cpu.reset();
+	ppu.reset();
+	nSystemClockCounter = 0;
+}
+
+void Bus::clock()
+{
+	ppu.clock();
+	if (nSystemClockCounter % 3 == 0)
+	{
+		cpu.clock();
+	}
+	if (ppu.nmi)
+	{
+		ppu.nmi = false;
+		cpu.nmi();
+	}
+
+	nSystemClockCounter++;
+}
---
+++ NES Emulator V2/Bus.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <cstdint>
+#include <array>
+
+#include "olc6502.h"
+#include "olc2C02.h"
+#include "Cartridge.h"
+
+class Bus
+{
+public:
+	Bus();
+	~Bus();
+
+public: // Devices on Main Bus
+
+	olc6502 cpu;
+	olc2C02 ppu;
+	std::shared_ptr<Cartridge> cart;
+	uint8_t cpuRam[2048];
+	uint8_t controller[2];
+
+public:
+	void    cpuWrite(uint16_t addr, uint8_t data);
+	uint8_t cpuRead(uint16_t addr, bool bReadOnly = false);
+
+private:
+	uint32_t nSystemClockCounter = 0;
+	uint8_t controller_state[2];
+
+public:
+	void insertCartridge(const std::shared_ptr<Cartridge>& cartridge);
+	void reset();
+	void clock();
+};
